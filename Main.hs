12160157774d196104771f67c3b6d25b53dbce80
Lucas Stadler <mi.au@papill0n.org>
2013-11-02 17:36:18 +0100

    Aaand now with an environment (and set! and define).
    `cond` is broken, though.

--- Main.hs
+++ Main.hs
@@ -5,6 +5,7 @@ import Data.Ratio
 import Data.List (find)
 import Control.Monad (liftM)
 import System.IO
+import Data.IORef
 import System.Environment (getArgs)
 
 import "mtl" Control.Monad.Error
@@ -86,6 +87,54 @@ trapError action = catchError action (return . show)
 
 extractValue :: ThrowsError a -> a
 extractValue (Right val) = val
+
+type Env = IORef [(String, IORef LispVal)]
+
+nullEnv :: IO Env
+nullEnv = newIORef []
+
+type IOThrowsError = ErrorT LispError IO
+
+liftThrows :: ThrowsError a -> IOThrowsError a
+liftThrows (Left err) = throwError err
+liftThrows (Right val) = return val
+
+runIOThrows :: IOThrowsError String -> IO String
+runIOThrows action = runErrorT (trapError action) >>= return . extractValue
+
+isBound :: Env -> String -> IO Bool
+isBound envRef var = readIORef envRef >>= return . maybe False (const True) . lookup var
+
+getVar :: Env -> String -> IOThrowsError LispVal
+getVar envRef var = do
+    env <- liftIO $ readIORef envRef
+    maybe (throwError $ UnboundVar "Getting an unbound variable" var)
+          (liftIO . readIORef)
+          (lookup var env)
+
+setVar :: Env -> String -> LispVal -> IOThrowsError LispVal
+setVar envRef var val = do
+    env <- liftIO $ readIORef envRef
+    maybe (throwError $ UnboundVar "Setting an unbound variable" var)
+          (liftIO . (flip writeIORef val))
+          (lookup var env)
+    return val
+
+defineVar :: Env -> String -> LispVal -> IOThrowsError LispVal
+defineVar envRef var val = do
+    alreadyDefined <- liftIO $ isBound envRef var
+    if alreadyDefined
+    then setVar envRef var val
+    else liftIO $ do
+        valRef <- newIORef val
+        env <- readIORef envRef
+        writeIORef envRef ((var, valRef):env)
+        return val
+
+bindVars :: Env -> [(String, LispVal)] -> IO Env
+bindVars envRef bindings = readIORef envRef >>= extendEnv bindings >>= newIORef
+    where extendEnv bindings env = liftM (++ env) (mapM addBinding bindings)
+          addBinding (var, val) = newIORef val >>= \valRef -> return (var, valRef)
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -200,20 +249,25 @@ readExpr input = case parse parseExpr "lieb" input of
     Left err -> throwError $ Parser err
     Right val -> return val
 
-eval :: LispVal -> ThrowsError LispVal
-eval val@(Char _) = return val
-eval val@(Bool _) = return val
-eval val@(String _) = return val
-eval val@(Float _) = return val
-eval val@(Number _) = return val
-eval (List [Atom "quote", val]) = return val
-eval (List [Atom "if", pred, conseq, alt]) = do
-    result <- eval pred
-    case result of
-        Bool False -> eval alt
-        otherwise  -> eval conseq
-eval (List (Atom fn : args)) = mapM eval args >>= apply fn
-eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
+eval :: Env -> LispVal -> IOThrowsError LispVal
+eval env val@(Char _) = return val
+eval env val@(Bool _) = return val
+eval env val@(String _) = return val
+eval env val@(Float _) = return val
+eval env val@(Number _) = return val
+eval env (Atom id) = getVar env id
+eval env (List [Atom "quote", val]) = return val
+eval env (List [Atom "if", pred, conseq, alt]) = do
+    result <- eval env pred
+    eval env $ case result of
+        Bool False -> alt
+        otherwise  -> conseq
+eval env (List [Atom "set!", Atom var, form]) =
+    eval env form >>= setVar env var
+eval env (List [Atom "define", Atom var, form]) =
+    eval env form >>= defineVar env var
+eval env (List (Atom fn : args)) = mapM (eval env) args >>= liftThrows . apply fn
+eval _ badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
 
 apply :: String -> [LispVal] -> ThrowsError LispVal
 apply fn args = maybe (throwError $ NotFunction "Unrecognized primitive function args" fn)
@@ -240,8 +294,7 @@ primitives = [("+", numericFn (+) 0),
     ("case", caseForm),
     ("car", car),
     ("cdr", cdr),
-    ("cons", cons),
-    ("eval", mapM eval >>= return . liftM head)]
+    ("cons", cons)]
 
 numericFn :: (Integer -> Integer -> Integer) -> Integer -> [LispVal] -> ThrowsError LispVal
 numericFn fn defaultVal [] = return $ Number defaultVal
@@ -310,7 +363,7 @@ cons [x, y] = return $ DottedList [x] y
 cons badArgList = throwError $ NumArgs 2 badArgList
 
 cond :: [LispVal] -> ThrowsError LispVal
-cond (c:cs) = makeConditional (c:cs) >>= eval
+cond (c:cs) = makeConditional (c:cs)
 cond badArgList = throwError $ NumArgs 1 badArgList
 
 makeConditional :: [LispVal] -> ThrowsError LispVal
@@ -326,11 +379,11 @@ makeConditional vals = do
 
 caseForm :: [LispVal] -> ThrowsError LispVal
 caseForm (key:[]) = return $ List [Atom "quote", List []]
-caseForm (key:(List (Atom "else":[alt])):_) = eval alt
+caseForm (key:(List (Atom "else":[alt])):_) = return alt
 caseForm (key:(List [cases, val]):cs) = do
     matches <- matchesClause key cases
     if matches
-    then eval val
+    then return val
     else caseForm $ key:cs
 
 matchesClause :: LispVal -> LispVal -> ThrowsError Bool
@@ -343,11 +396,11 @@ flushStr str = putStr str >> hFlush stdout
 readPrompt :: String -> IO String
 readPrompt prompt = flushStr prompt >> getLine
 
-evalString :: String -> IO String
-evalString expr = return . extractValue . trapError . liftM show $ readExpr expr >>= eval
+evalString :: Env -> String -> IO String
+evalString env expr = runIOThrows . liftM show $ (liftThrows $ readExpr expr) >>= eval env
 
-evalAndPrint :: String -> IO ()
-evalAndPrint expr = evalString expr >>= putStrLn
+evalAndPrint :: Env -> String -> IO ()
+evalAndPrint env expr = evalString env expr >>= putStrLn
 
 until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
 until_ pred prompt action = do
@@ -356,13 +409,16 @@ until_ pred prompt action = do
     then return ()
     else action result >> until_ pred prompt action
 
+runOne :: String -> IO ()
+runOne expr = nullEnv >>= flip evalAndPrint expr
+
 runRepl :: IO ()
-runRepl = until_ (== ":quit") (readPrompt "lieb> ") evalAndPrint
+runRepl = nullEnv >>= until_ (== ":quit") (readPrompt "lieb> ") . evalAndPrint
 
 main :: IO ()
 main = do
     args <- getArgs
     case length args of
         0 -> runRepl
-        1 -> evalAndPrint $ args !! 0
+        1 -> runOne $ args !! 0
         otherwise -> putStrLn "Usage: ./lieb or ./lieb [expr]"
